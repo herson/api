08a3ff8ea8c834987aa35a6621e0d5001db50c8c
Herson Cruz <herson@monroe.msei.biz>
2013-01-19 05:52:19 +0000

    v1 created

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -45,27 +45,33 @@ code {
 </head>
 <body>
 
-<h1>Welcome to CodeIgniter!</h1>
+<h1>MSEI API Service</h1>
 
-<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+<p>The page you are looking at is being generated dynamically.</p>
 
 <ul>
-	<li><a href="<?php echo site_url('api/example/users');?>">Users</a> - defaulting to XML</li>
-	<li><a href="<?php echo site_url('api/example/users/format/csv');?>">Users</a> - get it in CSV</li>
-	<li><a href="<?php echo site_url('api/example/user/id/1');?>">User #1</a> - defaulting to XML</li>
-	<li><a href="<?php echo site_url('api/example/user/id/1/format/json');?>">User #1</a> - get it in JSON</li>
-	<li><a id="ajax" href="<?php echo site_url('api/example/users/format/json');?>">Users</a> - get it in JSON (AJAX request)</li>
+	<li><a href="<?php echo site_url('example/users');?>">Users</a> - defaulting to XML</li>
+	<li><a href="<?php echo site_url('example/users/format/csv');?>">Users</a> - get it in CSV</li>
+	<li><a href="<?php echo site_url('example/user/id/1');?>">User #1</a> - defaulting to XML</li>
+	<li><a href="<?php echo site_url('example/user/id/1/format/json');?>">User #1</a> - get it in JSON</li>
+	<li><a id="ajaxu" href="<?php echo site_url('example/users/format/json');?>">Users</a> - get it in JSON (AJAX request)</li>
 </ul>
 
-<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+<ul>
+	<li><a href="<?php echo site_url('v1/emails');?>">Emails</a> - defaulting to XML</li>
+	<li><a href="<?php echo site_url('v1/emails/format/csv');?>">Emails</a> - get it in CSV</li>
+	<li><a href="<?php echo site_url('v1/email/id/1');?>">Email #1</a> - defaulting to XML</li>
+	<li><a href="<?php echo site_url('v1/email/id/1/format/json');?>">Email #1</a> - get it in JSON</li>
+	<li><a id="ajaxe" href="<?php echo site_url('v1/emails/format/json');?>">Emails</a> - get it in JSON (AJAX request)</li>
+</ul>
 
 <p><br />Page rendered in {elapsed_time} seconds</p>
 
 <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 <script type="text/javascript">
 $(function(){
-	// Bind a click event to the 'ajax' object id
-	$("#ajax").bind("click", function( evt ){
+	// Bind a click event to the 'ajaxu' object id
+	$("#ajaxu").bind("click", function( evt ){
 		// Javascript needs totake over. So stop the browser from redirecting the page
 		evt.preventDefault();
 		// AJAX request to get the data
@@ -83,6 +89,28 @@ $(function(){
 		});
 	});
 });
+
+$(function(){
+	// Bind a click event to the 'ajaxe' object id
+	$("#ajaxe").bind("click", function( evt ){
+		// Javascript needs totake over. So stop the browser from redirecting the page
+		evt.preventDefault();
+		// AJAX request to get the data
+		$.ajax({
+			// URL from the link that was clicked on
+			url: $(this).attr("href"),
+			// Success function. the 'data' parameter is an array of objects that can be looped over
+			success: function(data, textStatus, jqXHR){
+				alert('Successful AJAX request!');
+			}, 
+			// Failed to load request. This could be caused by any number of problems like server issues, bad links, etc. 
+			error: function(jqXHR, textStatus, errorThrown){
+				alert('Oh no! A problem with the AJAX request!');
+			}
+		});
+	});
+});
+
 </script>
 
 </body>
